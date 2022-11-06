277e69daad62d8e405fc9caae23cad8df49412d3
jayesh1602 <jayeshspatil1602@gmail.com>
2022-11-06 14:46:14 +0530

    Deploy attemp2

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.listen(5000, function () {
 });
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("frontend/build"));
   const path = require("path");
   app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
