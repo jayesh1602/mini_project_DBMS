1e35778c75bdc5d375ac2a002c514daf3b087a5c
jayesh1602 <jayeshspatil1602@gmail.com>
2022-11-06 11:00:46 +0530

    third

--- frontend/src/pages/mainPage.js
+++ frontend/src/pages/mainPage.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const MainPage = () => {
   return (
-    <div class="">
+    <div class="mainpage">
       <h1 class="text-center">Login Page</h1>
-      <div class="d-flex justify-content-center">
+      <div class="main-buttons d-flex justify-content-center">
         <button type="button" class="btn btn-primary">
           Admin
         </button>
-        <button type="button" className="btn btn-success ml-4">
+        <button type="button" className="main-btn btn btn-success ml-4">
           Student
         </button>
       </div>
